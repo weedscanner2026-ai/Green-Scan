2e2cc2b777d9c73991fdd6aaa6feee0b21cbe715
WeedDetector <kaitharevilla@gmail.com>
2026-03-10 15:34:46 +0800

    Use Pillow 9.5.0 with pre-built wheels

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Render build script
 
-echo "Installing system dependencies for Pillow..."
-apt-get update
-apt-get install -y libjpeg-dev zlib1g-dev
-
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
