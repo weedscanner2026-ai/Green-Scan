4914456b37d935ccf1f0d4ac94936cb4a223c45e
WeedDetector <kaitharevilla@gmail.com>
2026-03-10 15:26:49 +0800

    Add Pillow build dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Render build script
 
+echo "Installing system dependencies for Pillow..."
+apt-get update
+apt-get install -y libjpeg-dev zlib1g-dev
+
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
